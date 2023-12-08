772d0e847b61d0dc3970d287c25dc33837739cf5
jonas <jonasleonhardfl@gmail.com>
2023-12-08 18:37:32 +0100

    fix(day-03): added parsing to part1 (#WIP)

--- 2023/rust/day-03/src/part1.rs
+++ 2023/rust/day-03/src/part1.rs
@@ -1,7 +1,11 @@
 use std::ops::Range;
 
-use nom::IResult;
+use nom::{
+    bytes::complete::take_while, character::complete::digit1, multi::separated_list0, IResult,
+    Offset,
+};
 
+#[derive(Debug)]
 struct Number {
     line_nr: usize,
     positions: Range<usize>,
@@ -9,30 +13,87 @@ struct Number {
 }
 
 fn parse_numbers(line_nr: usize, line: &str) -> IResult<&str, Vec<Number>> {
-    todo!()
+    let is_dot = |c: char| c == '.';
+    let (remaining, results) = separated_list0(take_while(is_dot), digit1)(line)?;
+
+    println!("results after parsing for {:?}::: {:?}", line, results);
+    Ok((
+        remaining,
+        results
+            .into_iter()
+            .map(|result| {
+                let start = line.offset(result);
+                let end = start + result.len();
+                let value = result.parse::<u32>().unwrap(); // handle this unwrap properly
+                Number {
+                    line_nr,
+                    positions: start..end,
+                    value,
+                }
+            })
+            .collect(),
+    ))
+}
+
+fn check_is_symbol(line_nr: usize, x: usize, lines: &[&str]) -> bool {
+    if let Some(line) = lines.get(line_nr) {
+        if let Some(char) = line.chars().nth(x) {
+            return !matches!(char, '.');
+        }
+    }
+
+    false
 }
 
 pub fn solve(input: &str) -> Result<String, String> {
-    // go through all lines, collect adjacent numbers into vec of Number
-
-    let lines = input.lines();
+    let lines: Vec<&str> = input.lines().collect();
 
     let numbers: Vec<Number> = lines
+        .iter()
         .enumerate()
         .flat_map(|(index, line)| {
-            let (_, numbers) = parse_numbers(index, line).unwrap();
+            let (_, numbers) = parse_numbers(index, line).unwrap_or((line, vec![]));
             numbers
         })
         .collect();
 
+    println!("numbers: {:?}", numbers);
+
     let numbers_next_to_sign = numbers.iter().filter(|number| {
         for x in number.positions.start..number.positions.end {
             // check if there is a sign at the top or bottom of the x position in the number
+            let has_top_symbol = check_is_symbol(number.line_nr - 1, x, &lines);
+            let has_bottom_symbol = check_is_symbol(number.line_nr + 1, x, &lines);
+
+            if has_top_symbol || has_bottom_symbol {
+                return true;
+            }
         }
-        // check if there is a sign left to start or right of end
+        // check if there is a sign left to start or right of end of diagonal
+        let has_left_symbol = check_is_symbol(number.line_nr, number.positions.start - 1, &lines);
+        let has_left_top_symbol =
+            check_is_symbol(number.line_nr + 1, number.positions.start - 1, &lines);
+        let has_left_bot_symbol =
+            check_is_symbol(number.line_nr - 1, number.positions.start - 1, &lines);
+
+        let has_right_symbol = check_is_symbol(number.line_nr, number.positions.end + 1, &lines);
+        let has_right_top_symbol =
+            check_is_symbol(number.line_nr + 1, number.positions.end + 1, &lines);
+        let has_right_bot_symbol =
+            check_is_symbol(number.line_nr - 1, number.positions.end + 1, &lines);
+
+        if has_left_symbol
+            || has_left_top_symbol
+            || has_left_bot_symbol
+            || has_right_symbol
+            || has_right_top_symbol
+            || has_right_bot_symbol
+        {
+            return true;
+        }
 
         // if one of those is true, filter out this number
-        todo!()
+        false
     });
 
     let sum_of_numbers = numbers_next_to_sign.map(|number| number.value).sum::<u32>();
